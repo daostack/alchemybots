d700302f84888f3ac3a9e220e803634e3a76e964
Ben <ben.kaufman10@gmail.com>
2020-01-23 14:24:59 +0200

    Use executeBoosted for `call`

--- index.js
+++ index.js
@@ -233,7 +233,7 @@ async function setExecutionTimer(genesisProtocol, proposalId, timerDelay) {
     // Check if can close the proposal as expired and claim the bounty
     let failed = false;
     let expirationCallBounty = await genesisProtocol.methods
-      .execute(proposalId)
+      .executeBoosted(proposalId)
       .call()
       .catch(error => {
         log(
