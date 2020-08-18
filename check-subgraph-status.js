b1bcac4efa0080d350a87135b7dfdea32f2870ca
benk10 <ben@daostack.io>
2020-08-18 14:10:43 +0200

    Set subgraph URL temporarily

--- check-subgraph-status.js
+++ check-subgraph-status.js
@@ -56,9 +56,10 @@ async function updateAlchemySettings() {
   );
   let alchemySettings = require('./alchemy-settings.json');
   if (alchemySettings.http_main !== GRAPH_NODE_SUBGRAPH_URL && GRAPH_NODE_SUBGRAPH_URL !== '') {
-    sendAlchemySwitchedSubgraph(GRAPH_NODE_SUBGRAPH_URL, alchemySettings.http_main)
+    // sendAlchemySwitchedSubgraph(GRAPH_NODE_SUBGRAPH_URL, alchemySettings.http_main)
   }
-  GRAPH_NODE_SUBGRAPH_URL = alchemySettings.http_main;
+  // GRAPH_NODE_SUBGRAPH_URL = alchemySettings.http_main;
+  GRAPH_NODE_SUBGRAPH_URL = 'https://api.thegraph.com/subgraphs/name/daostack/v39_4';
   
   console.log('Alchemy production subgraph URL: ' + GRAPH_NODE_SUBGRAPH_URL);
 }
